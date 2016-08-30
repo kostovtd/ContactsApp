31f6b7645e279a0320374a482ad3d0844dceaa7f
Todor Kostov <kostovtd@gmail.com>
2016-08-30 13:00:20 +0300

    Real DB included Contact POJO included

--- app/src/main/java/com/contactsapp/MainActivity.java
+++ app/src/main/java/com/contactsapp/MainActivity.java
@@ -2,12 +2,35 @@ package com.contactsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getCanonicalName();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCrate hit");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/contactsapp/models/Contact.java
@@ -0,0 +1,56 @@
+package com.contactsapp.models;
+
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Contact extends RealmObject{
+
+    @PrimaryKey
+    private String phoneNumber;
+
+    private String firstName;
+    private String lastName;
+    private String address;
+
+
+    public Contact(String firstName, String lastName, String phoneNumber, String address) {
+        this.phoneNumber = phoneNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
